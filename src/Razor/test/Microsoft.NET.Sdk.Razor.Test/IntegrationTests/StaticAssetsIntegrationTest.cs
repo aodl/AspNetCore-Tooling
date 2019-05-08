d0bc729a8f47d21ab82ad8d41f9e5ce06985323f
Javier Calvarro Nelson <jacalvar@microsoft.com>
2019-05-08 05:37:40 -0700

    Add tests for clean and rebuild

--- src/Razor/test/Microsoft.NET.Sdk.Razor.Test/IntegrationTests/StaticAssetsIntegrationTest.cs
+++ src/Razor/test/Microsoft.NET.Sdk.Razor.Test/IntegrationTests/StaticAssetsIntegrationTest.cs
@@ -30,6 +30,67 @@ namespace Microsoft.AspNetCore.Razor.Design.IntegrationTests
 
         [Fact]
         [InitializeTestProject("AppWithPackageAndP2PReference")]
+        public async Task Clean_Success_RemovesManifestAndCache()
+        {
+            var result = await DotnetMSBuild("Build");
+
+            Assert.BuildPassed(result);
+
+            // GenerateAspNetCoreStaticAssetsManifest should generate the manifest and the cache.
+            Assert.FileExists(result, IntermediateOutputPath, "Microsoft.AspNetCore.StaticAssets.xml");
+            Assert.FileExists(result, IntermediateOutputPath, "Microsoft.AspNetCore.StaticAssets.cache");
+
+            var cleanResult = await DotnetMSBuild("Clean");
+
+            Assert.BuildPassed(cleanResult);
+
+            // Clean should delete the manifest and the cache.
+            Assert.FileDoesNotExist(result, IntermediateOutputPath, "Microsoft.AspNetCore.StaticAssets.cache");
+            Assert.FileDoesNotExist(result, IntermediateOutputPath, "Microsoft.AspNetCore.StaticAssets.xml");
+        }
+
+        [Fact]
+        [InitializeTestProject("AppWithPackageAndP2PReference")]
+        public async Task Rebuild_Success_RecreatesManifestAndCache()
+        {
+            // Arrange
+            var result = await DotnetMSBuild("Build");
+
+            Assert.BuildPassed(result);
+
+            // GenerateAspNetCoreStaticAssetsManifest should generate the manifest and the cache.
+            Assert.FileExists(result, IntermediateOutputPath, "Microsoft.AspNetCore.StaticAssets.xml");
+            Assert.FileExists(result, IntermediateOutputPath, "Microsoft.AspNetCore.StaticAssets.cache");
+
+            var directoryPath = Path.Combine(result.Project.DirectoryPath, IntermediateOutputPath);
+            var thumbPrints = new Dictionary<string, FileThumbPrint>();
+            var thumbPrintFiles = new[]
+            {
+                Path.Combine(directoryPath, "Microsoft.AspNetCore.StaticAssets.xml"),
+                Path.Combine(directoryPath, "Microsoft.AspNetCore.StaticAssets.cache"),
+            };
+
+            foreach (var file in thumbPrintFiles)
+            {
+                var thumbprint = GetThumbPrint(file);
+                thumbPrints[file] = thumbprint;
+            }
+
+            // Act
+            var rebuild = await DotnetMSBuild("Rebuild");
+
+            // Assert
+            Assert.BuildPassed(rebuild);
+
+            foreach (var file in thumbPrintFiles)
+            {
+                var thumbprint = GetThumbPrint(file);
+                Assert.NotEqual(thumbPrints[file], thumbprint);
+            }
+        }
+
+        [Fact]
+        [InitializeTestProject("AppWithPackageAndP2PReference")]
         public async Task GenerateAspNetCoreStaticAssetsManifest_IncrementalBuild_ReusesManifest()
         {
             // Arrange
